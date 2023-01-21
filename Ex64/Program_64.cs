1cb96891c036ba7b10fce5405511c2a8fefe62a7
«Nikolay <antonooff_2nic@mail.ru>
2023-01-21 13:22:58 +0800

    initial commit

---
+++ Ex64/Program_64.cs
@@ -0,0 +1,19 @@
+﻿// 
+// Задача 64: Задайте значение N. Напишите программу, которая 
+// выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// 
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine(printNum(N));
+string printNum(int N)
+{
+    if (N == 1)
+    {
+        return N.ToString();
+    }
+    else
+    {
+        return (N + " " + printNum(N - 1));
+    }
+}
---
+++ Ex68/Program_68.cs
@@ -0,0 +1,18 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// 
+Console.Write("Введите число m: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число n: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Aker(n, m));
+int Aker(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return Aker(n - 1, 1);
+    else
+      return Aker(n - 1, Aker(n, m - 1));
+}
